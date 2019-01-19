c7dcc4f2c3e8ee0ed2c9d6f029d20ed56f41cc3a
Erick Rocha <contato@erickpatrick.net>
2019-01-19 01:06:55 +0100

    updates admin file to not extend any template

--- source/admin.blade.php
+++ source/admin.blade.php
@@ -1,20 +1,24 @@
-@extends('_layouts.admin')
-
-@push('meta')
-<script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-@endpush
-
-@push('scriptsBeforeBodyend')
-<script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script>
-<script>
-if (window.netlifyIdentity) {
-    window.netlifyIdentity.on("init", user => {
-        if (!user) {
-            window.netlifyIdentity.on("login", () => {
-                document.location.href = "/admin/";
-            });
-        }
-    });
-}
-</script>
-@endpush
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Admin | {{ $page->site->title }}</title>
+    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+</head>
+<body>
+    <script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script>
+    <script>
+    if (window.netlifyIdentity) {
+        window.netlifyIdentity.on("init", user => {
+            if (!user) {
+                window.netlifyIdentity.on("login", () => {
+                    document.location.href = "/admin/";
+                });
+            }
+        });
+    }
+    </script>
+</body>
+</html>
